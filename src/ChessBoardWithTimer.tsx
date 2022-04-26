377a35d6adab044213033093487fca9c7c3f95c2
David Veszelovszki <veszelovszki@gmail.com>
2022-04-26 10:38:10 +0200

    Set timer expiration to end the game

--- src/ChessBoardWithTimer.tsx
+++ src/ChessBoardWithTimer.tsx
@@ -19,27 +19,34 @@ enum Result {
     Draw = 'draw',
 }
 
+const gameLengthMs = 0.2 * 60 * 1000
+
 export default function ChessBoardWithTimer({ lightIndex, darkIndex, lightPlayerName, darkPlayerName }: Props) {
     const [game, setGame] = useState<Chess>(new Chess())
     const [remoteBoardStatus, setRemoteBoardStatus] = useState<BoardStatus | undefined>(undefined)
     const [lightElapsedMs, setLightElapsedMs] = useState(0)
     const [darkElapsedMs, setDarkElapsedMs] = useState(0)
+    const [hasLightResigned, setHasLightResigned] = useState(false)
+    const [hasDarkResigned, setHasDarkResigned] = useState(false)
 
     const elapsedMsSinceLastUpdate = remoteBoardStatus ? (new Date().getTime() - new Date(remoteBoardStatus.lastUpdateDateTime).getTime()) : undefined
     const isLightTurn: boolean = game.turn() === 'w'
-    const gameResult: Result = game.game_over()
-        ? (game.in_checkmate() ? (game.turn() === 'w' ? Result.Dark : Result.Light) : Result.Draw)
-        : Result.Running
+    const gameResult = getGameResult(game, hasLightResigned, hasDarkResigned)
 
     useEffect(() => {
         const interval = setInterval(() => {
             if (remoteBoardStatus && elapsedMsSinceLastUpdate) {
                 setLightElapsedMs(remoteBoardStatus.lightElapsedMs + (isLightTurn ? elapsedMsSinceLastUpdate : 0))
                 setDarkElapsedMs(remoteBoardStatus.darkElapsedMs + (!isLightTurn ? elapsedMsSinceLastUpdate : 0))
+                if (lightElapsedMs > gameLengthMs) {
+                    setHasLightResigned(true)
+                } else if (darkElapsedMs > gameLengthMs) {
+                    setHasDarkResigned(true)
+                }
             }
         }, 70)
         return () => clearInterval(interval)
-    }, [remoteBoardStatus, elapsedMsSinceLastUpdate, isLightTurn])
+    }, [remoteBoardStatus, elapsedMsSinceLastUpdate, isLightTurn, lightElapsedMs, darkElapsedMs])
 
     /* Update every second */
     useEffect(() => {
@@ -68,6 +75,10 @@ export default function ChessBoardWithTimer({ lightIndex, darkIndex, lightPlayer
     }
 
     function onDrop(sourceSquare: Square, targetSquare: Square) {
+        if (game.game_over() || hasLightResigned || hasDarkResigned) {
+            return false
+        }
+
         let move = null
         makeMoveAndSaveState((game: Chess) => {
             move = game.move({
@@ -81,11 +92,22 @@ export default function ChessBoardWithTimer({ lightIndex, darkIndex, lightPlayer
     }
 
     return <div>
-        <Timer player={darkPlayerName} isLight={false} targetMs={20 * 60 * 1000} elapsedMs={darkElapsedMs} />
+        <Timer player={darkPlayerName} isLight={false} targetMs={gameLengthMs} elapsedMs={darkElapsedMs} />
         <Chessboard id={lightIndex * 100 + darkIndex} position={game.fen()} onPieceDrop={onDrop} boardWidth={200} />
-        <Timer player={lightPlayerName} isLight={true} targetMs={20 * 60 * 1000} elapsedMs={lightElapsedMs} />
-        {(gameResult !== Result.Running) &&
-            <span>{gameResult === Result.Light ? `${lightPlayerName} won!` : (gameResult === Result.Dark ? `${darkPlayerName} won!` : 'It’s a draw!')}</span>
-        }
+        <Timer player={lightPlayerName} isLight={true} targetMs={gameLengthMs} elapsedMs={lightElapsedMs} />
+        <div style={{textAlign: 'center'}}>{(gameResult !== Result.Running) ?
+            gameResult === Result.Light
+                ? `${lightPlayerName} won!`
+                : (gameResult === Result.Dark ? `${darkPlayerName} won!`
+                    : 'It’s a draw!')
+            : ' '}</div>
     </div>
+}
+
+function getGameResult(game: Chess, hasLightResigned: boolean, hasDarkResigned: boolean): Result {
+    return (game.game_over() || hasLightResigned || hasDarkResigned)
+        ? ((game.in_checkmate() && game.turn() === 'w') || hasLightResigned) ? Result.Dark
+            : (((game.in_checkmate() && game.turn() === 'b') || hasDarkResigned) ? Result.Light
+                : Result.Draw)
+        : Result.Running
 }
--- src/Timer.tsx
+++ src/Timer.tsx
@@ -4,10 +4,10 @@ type Props = {
     targetMs: number
     elapsedMs: number
 }
-export default function Timer({player, isLight, targetMs, elapsedMs}: Props) {
-    const remaining = targetMs - elapsedMs;
+export default function Timer({ player, isLight, targetMs, elapsedMs }: Props) {
+    const remaining = Math.max(Math.min(targetMs - elapsedMs, targetMs), 0)
     const minutes = Math.floor(remaining / (1000 * 60)).toString().padStart(2, '0')
     const seconds = Math.floor(remaining % (1000 * 60) / 1000).toString().padStart(2, '0')
     const milliseconds = (remaining % 1000).toString().padStart(3, '0')
-    return <div className={isLight ? 'light' : 'dark'}>{player}: {minutes}:{seconds}.{milliseconds}</div>
+    return <div className={isLight ? 'light' : 'dark'}>{player}: <big>{minutes}</big>:{seconds}<small>.{milliseconds}</small></div>
 }
